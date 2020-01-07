221bee16743d1f586c5cfcffe3b5a020a306770c
Michal Habera <michal.habera@gmail.com>
2020-01-07 16:38:11 +0100

    Add intial setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dolfiny',
+      version='0.1',
+      description='Dolfin-y high level wrappers for dolfin-x',
+      author='Michal Habera',
+      author_email='michal.habera@gmail.com',
+      license='MIT',
+      packages=['dolfin-y'],
+      zip_safe=False)
