78cfccfcb2225dafa6a093c887991694b9d9c6fd
Michal Habera <michal.habera@gmail.com>
2020-01-07 17:58:18 +0100

    Add simple test

---
+++ dolfiny/function.py
@@ -0,0 +1,43 @@
+
+import numpy as np
+import ufl
+from ufl.corealg.multifunction import MultiFunction
+from ufl.algorithms.map_integrands import map_integrand_dags
+
+
+class Replacer(MultiFunction):
+    def __init__(self, mapping):
+        MultiFunction.__init__(self)
+        self._mapping = mapping
+
+    def expr(self, o, *args):
+        if o in self._mapping:
+            return self._mapping[o]
+        else:
+            return self.reuse_if_untouched(o, *args)
+
+
+def extract_blocks(form, test_functions, trial_functions):
+    # Prepare empty block matrices list
+    blocks = [[None for i in  range(len(test_functions))] for j in  range(len(trial_functions))]
+
+    for i, tef in enumerate(test_functions):
+        for j, trf in enumerate(trial_functions):
+            to_null = dict()
+
+            # Dictionary mapping the other trial functions
+            # to zero
+            for item in trial_functions:
+                if item != trf:
+                    to_null[item] = ufl.zero(item.ufl_shape)
+
+            # Dictionary mapping the other test functions
+            # to zero
+            for item in test_functions:
+                if item != tef:
+                    to_null[item] = ufl.zero(item.ufl_shape)
+
+            replacer = Replacer(to_null)
+            blocks[i][j] = map_integrand_dags(replacer, form)
+
+    return blocks
---
+++ test/test_function.py
@@ -0,0 +1,32 @@
+import dolfin
+import ufl
+import dolfiny.function
+import pytest
+import pdb
+
+
+@pytest.fixture
+def mesh():
+    return dolfin.generation.UnitSquareMesh(dolfin.MPI.comm_world, 5, 5)
+
+
+@pytest.fixture
+def V1(mesh):
+    return dolfin.FunctionSpace(mesh, ("P", 1))
+
+
+@pytest.fixture
+def V2(mesh):
+    return dolfin.FunctionSpace(mesh, ("P", 2))
+
+
+def test_ufl_block_extraction(V1, V2, mesh):
+    u1, v1 = ufl.TrialFunction(V1), ufl.TestFunction(V1)
+    u2, v2 = ufl.TrialFunction(V2), ufl.TestFunction(V2)
+
+    a = u1 * v1 * ufl.dx(mesh) + ufl.grad(u1 * v2)[0] * ufl.dx(mesh)
+
+    ablocks = dolfiny.function.extract_blocks(a, [v1, v2], [u1, u2])
+
+    assert ablocks[1][1].empty()
+    assert ablocks[0][1].empty()
