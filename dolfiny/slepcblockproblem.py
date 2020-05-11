a1a237a89580e573ddb18a4ebf1d3d53781edbdb
Michal Habera <michal.habera@gmail.com>
2020-05-11 14:21:02 +0200

    Add initial SLEPc block problem version

---
+++ dolfiny/slepcblockproblem.py
@@ -0,0 +1,105 @@
+import typing
+
+import dolfinx
+import ufl
+from dolfiny.function import functions_to_vec, vec_to_functions
+from slepc4py import SLEPc
+
+
+class SLEPcBlockProblem():
+    def __init__(self, F_form: typing.List, u: typing.List, lmbda: dolfinx.Function,
+                 A_form=None, B_form=None, prefix=None):
+        """SLEPc problem and solver wrapper.
+
+        Wrapper for a generalised eigenvalue problem obtained from UFL residual forms.
+        """
+        self.F_form = F_form
+        self.u = u
+        self.lmbda = lmbda
+        self.comm = u[0].function_space.mesh.mpi_comm()
+
+        self.ur = []
+        self.ui = []
+        for func in u:
+            self.ur.append(dolfinx.function.Function(func.function_space, name=func.name))
+            self.ui.append(dolfinx.function.Function(func.function_space, name=func.name))
+
+        # Prepare tangent form M0 which has terms involving lambda
+        self.M0 = [[None for i in range(len(self.u))] for j in range(len(self.u))]
+        for i in range(len(self.u)):
+            for j in range(len(self.u)):
+                self.M0[i][j] = ufl.algorithms.expand_derivatives(ufl.derivative(
+                    F_form[i], self.u[j], ufl.TrialFunction(self.u[j].function_space)))
+
+        if A_form is None:
+            A0 = [[None for i in range(len(self.u))] for j in range(len(self.u))]
+
+            for i in range(len(self.u)):
+                for j in range(len(self.u)):
+                    # If the form happens to be empty replace with None
+                    if self.M0[i][j].empty():
+                        A0[i][j] = None
+                        continue
+
+                    # Differentiate wrt. lambda and replace all remaining lambda with Zero
+                    A0[i][j] = ufl.algorithms.expand_derivatives(ufl.diff(self.M0[i][j], lmbda))
+                    A0[i][j] = ufl.replace(A0[i][j], {lmbda: ufl.zero()})
+            self.A_form = A0
+        else:
+            self.A_form = A_form
+
+        if B_form is None:
+            B0 = [[None for i in range(len(self.u))] for j in range(len(self.u))]
+
+            for i in range(len(self.u)):
+                for j in range(len(self.u)):
+                    B0[i][j] = ufl.replace(self.M0[i][j], {lmbda: ufl.zero()})
+
+                    if B0[i][j].empty():
+                        B0[i][j] = None
+                        continue
+            self.B_form = B0
+        else:
+            self.B_form = B_form
+
+        self.eps = SLEPc.EPS().create(self.comm)
+        self.eps.setOptionsPrefix(prefix)
+
+        self.A = dolfinx.fem.create_matrix_block(self.A_form)
+
+        self.B = None
+        if not self.empty_B():
+            self.B = dolfinx.fem.create_matrix_block(self.B_form)
+
+        self.eps.setProblemType(SLEPc.EPS.ProblemType.HEP)
+        self.eps.setFromOptions()
+
+    def solve(self):
+        self.A.zeroEntries()
+        dolfinx.fem.assemble_matrix_block(self.A, self.A_form, [])
+        self.A.assemble()
+
+        if not self.empty_B():
+            self.B.zeroEntries()
+            dolfinx.fem.assemble_matrix_block(self.B, self.B_form, [])
+            self.B.assemble()
+
+        self.eps.setOperators(self.A, self.B)
+        self.eps.solve()
+
+    def getEigenpair(self, i):
+        xr, xi = self.A.getVecs()
+        eigval = self.eps.getEigenpair(i, xr, xi)
+
+        vec_to_functions(xr, self.ur)
+        vec_to_functions(xi, self.ui)
+
+        return (eigval, self.ur, self.ui)
+
+    def empty_B(self):
+        for i in range(len(self.B_form)):
+            for j in range(len(self.B_form[i])):
+                if self.B_form[i][j] is not None:
+                    return False
+
+        return True
---
+++ test/test_slepcproblem.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+import dolfiny.slepcblockproblem
+import dolfiny.la
+
+import ufl
+from dolfinx.function import Function, FunctionSpace, VectorFunctionSpace, TensorFunctionSpace, Constant
+from dolfinx.generation import UnitCubeMesh
+from dolfinx.io import XDMFFile
+from mpi4py import MPI
+from petsc4py import PETSc
+from scipy.sparse import linalg
+
+
+def test_neohooke():
+    mesh = UnitCubeMesh(MPI.COMM_WORLD, 7, 7, 7)
+    V = VectorFunctionSpace(mesh, ("P", 1))
+    P = FunctionSpace(mesh, ("P", 1))
+
+    L = FunctionSpace(mesh, ("R", 0))
+
+    u = Function(V, name="u")
+    v = ufl.TestFunction(V)
+
+    p = Function(P, name="p")
+    q = ufl.TestFunction(P)
+
+    lmbda0 = Function(L)
+
+    d = mesh.topology.dim
+    Id = ufl.Identity(d)
+    F = Id + ufl.grad(u)
+    C = F.T * F
+    J = ufl.det(F)
+
+    E_, nu_ = 10.0, 0.3
+    mu, lmbda = E_ / (2 * (1 + nu_)), E_ * nu_ / ((1 + nu_) * (1 - 2 * nu_))
+    psi = (mu / 2) * (ufl.tr(C) - 3) - mu * ufl.ln(J) + lmbda / 2 * ufl.ln(J) ** 2 + (p - 1.0) ** 2
+    pi = lmbda0 * psi * ufl.dx
+
+    F0 = ufl.derivative(pi, u, v)
+    F1 = ufl.derivative(pi, p, q)
+
+    # Number of eigenvalues to find
+    nev = 10
+
+    opts = PETSc.Options("neohooke")
+    opts["eps_smallest_magnitude"] = True
+    opts["eps_nev"] = nev
+    opts["eps_ncv"] = 50 * nev
+    opts["eps_conv_abs"] = True
+
+    # opts["eps_non_hermitian"] = True
+    opts["eps_tol"] = 1.0e-14
+    opts["eps_max_it"] = 1000
+    opts["eps_error_relative"] = "::ascii_info_detail"
+    opts["eps_monitor"] = "::ascii_info_detail"
+
+    slepcp = dolfiny.slepcblockproblem.SLEPcBlockProblem([F0, F1], [u, p], lmbda0, prefix="neohooke")
+    slepcp.solve()
+
+    slepcp.eps.view()
+
+    # mat = dolfiny.la.petsc_to_scipy(slepcp.eps.getOperators()[0])
+    # eigvals, eigvecs = linalg.eigsh(mat, which="SM", k=nev)
+
+    with XDMFFile(MPI.COMM_WORLD, "eigvec.xdmf", "w") as ofile:
+        ofile.write_mesh(mesh)
+        for i in range(nev):
+            eigval, ur, ui = slepcp.getEigenpair(i)
+
+            # Expect first 6 eignevalues 0, i.e. rigid body modes
+            if i < 6:
+                assert np.isclose(eigval, 0.0)
+
+            for func in ur:
+                name = func.name
+                func.name = f"{name}_eigvec_{i}_real"
+                ofile.write_function(func)
+
+                func.name = name
