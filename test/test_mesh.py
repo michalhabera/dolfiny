9497f2bcb7fba3bce6676c00518accdf629e3a8c
Michal Habera <michal.habera@gmail.com>
2020-05-23 15:20:43 +0200

    Resurrect gmsh-to-dolfin

--- test/test_mesh.py
+++ test/test_mesh.py
@@ -1,54 +1,59 @@
-# import numpy
-# from mpi4py import MPI
+import numpy
+from mpi4py import MPI
 
-# import dolfinx
-# import dolfinx.io
-# import dolfiny.mesh
-# import pytest
-# import ufl
-
-# skip_in_parallel = pytest.mark.skipif(
-#     MPI.COMM_WORLD.size > 1,
-#     reason="This test should only be run in serial.")
+import dolfinx
+from dolfinx.io import XDMFFile
+import dolfiny.mesh
+import pytest
+import ufl
 
 
-# @skip_in_parallel
-# def test_simple_triangle():
-#     import gmsh
+def test_simple_triangle():
+    import gmsh
 
-#     gmsh.initialize()
-#     gmsh.model.add("test")
+    gmsh.initialize()
+    gmsh.model.add("test")
 
-#     p0 = gmsh.model.geo.addPoint(0.0, 0.0, 0.0)
-#     p1 = gmsh.model.geo.addPoint(1.0, 0.0, 0.0)
-#     p2 = gmsh.model.geo.addPoint(1.0, 1.0, 0.0)
-#     p3 = gmsh.model.geo.addPoint(0.0, 1.0, 0.0)
-#     p4 = gmsh.model.geo.addPoint(1.0, 0.5, 0.0)
+    p0 = gmsh.model.geo.addPoint(0.0, 0.0, 0.0)
+    p1 = gmsh.model.geo.addPoint(1.0, 0.0, 0.0)
+    p2 = gmsh.model.geo.addPoint(1.0, 1.0, 0.0)
+    p3 = gmsh.model.geo.addPoint(0.0, 1.0, 0.0)
+    p4 = gmsh.model.geo.addPoint(1.0, 0.5, 0.0)
 
-#     l0 = gmsh.model.geo.addLine(p0, p1)
-#     l1 = gmsh.model.geo.addCircleArc(p1, p4, p2)
-#     l2 = gmsh.model.geo.addLine(p2, p3)
-#     l3 = gmsh.model.geo.addLine(p3, p0)
+    l0 = gmsh.model.geo.addLine(p0, p1)
+    l1 = gmsh.model.geo.addCircleArc(p1, p4, p2)
+    l2 = gmsh.model.geo.addLine(p2, p3)
+    l3 = gmsh.model.geo.addLine(p3, p0)
 
-#     cl0 = gmsh.model.geo.addCurveLoop([l0, l1, l2, l3])
-#     s0 = gmsh.model.geo.addPlaneSurface([cl0])
+    cl0 = gmsh.model.geo.addCurveLoop([l0, l1, l2, l3])
+    s0 = gmsh.model.geo.addPlaneSurface([cl0])
 
-#     gmsh.model.addPhysicalGroup(1, [l0, l2], 2)
-#     gmsh.model.addPhysicalGroup(1, [l1], 3)
+    gmsh.model.addPhysicalGroup(1, [l0, l2], 2)
+    gmsh.model.setPhysicalName(1, 2, "sides")
 
-#     gmsh.model.addPhysicalGroup(2, [s0], 4)
+    gmsh.model.addPhysicalGroup(1, [l1], 3)
+    gmsh.model.setPhysicalName(1, 3, "arc")
 
-#     gmsh.model.geo.synchronize()
-#     gmsh.model.mesh.generate()
-#     gmsh.model.mesh.refine()
+    gmsh.model.addPhysicalGroup(2, [s0], 4)
+    gmsh.model.setPhysicalName(2, 4, "surface")
 
-#     mesh, mvcs = dolfiny.mesh.gmsh_to_dolfin(gmsh.model, 2, prune_z=True)
+    gmsh.model.geo.synchronize()
+    gmsh.model.mesh.generate()
+    gmsh.model.mesh.setOrder(2)
 
-#     assert mesh.geometry.dim == 2
-#     assert mesh.topology.dim == 2
+    mesh, mts = dolfiny.mesh.gmsh_to_dolfin(gmsh.model, 2, prune_z=True)
 
-#     mf = dolfinx.MeshFunction("size_t", mesh, mvcs[(1, 3)], 0)
-#     ds = ufl.Measure("ds", subdomain_data=mf, domain=mesh)
+    assert mesh.geometry.dim == 2
+    assert mesh.topology.dim == 2
+    assert mts["arc"].dim == 1
 
-#     val = dolfinx.fem.assemble_scalar(1.0 * ds(3))
-#     assert numpy.isclose(val, numpy.pi / 2.0, rtol=1.0e-3)
+    with XDMFFile(MPI.COMM_WORLD, "mesh.xdmf", "w") as file:
+        file.write_mesh(mesh)
+        mesh.topology.create_connectivity(1, 2)
+        file.write_meshtags(mts["arc"])
+
+    ds_arc = ufl.Measure("ds", subdomain_data=mts["arc"], domain=mesh, subdomain_id=3)
+
+    val = dolfinx.fem.assemble_scalar(1.0 * ds_arc)
+    val = mesh.mpi_comm().allreduce(val, op=MPI.SUM)
+    assert numpy.isclose(val, 2.0 * numpy.pi * 0.5 / 2.0, rtol=1.0e-3)
