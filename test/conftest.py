209fcefe80e3e886832814dd48ff5cb249414285
Michal Habera <michal.habera@gmail.com>
2020-05-23 17:49:44 +0200

    Try adding dolfinx teardown

--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,23 @@ import dolfinx
 import pytest
 
 
+def pytest_runtest_teardown(item):
+    """Collect garbage after every test to force calling
+    destructors which might be collective"""
+
+    # Do the normal teardown
+    item.teardown()
+
+    # Collect the garbage (call destructors collectively)
+    del item
+    # NOTE: How are we sure that 'item' does not hold references
+    #       to temporaries and someone else does not hold a reference
+    #       to 'item'?! Well, it seems that it works...
+    gc.collect()
+    comm = MPI.COMM_WORLD
+    comm.Barrier()
+
+
 @pytest.fixture(scope="module")
 def squaremesh_5():
     return dolfinx.generation.UnitSquareMesh(MPI.COMM_WORLD, 5, 5)
