8e25bf50a0c13cfda43b34b4658665739d6e781f
Andreas Zilian <andreas.zilian@uni.lu>
2020-05-26 09:18:59 +0200

    Sub-class XDMFFile in dolfiny

--- dolfiny/io.py
+++ dolfiny/io.py
@@ -1,72 +1,75 @@
 import logging
 
-
-def write(xdmf_file, mesh, mts=None):
-    """Write mesh and meshtags to XDMFFile
-
-    Parameters
-    ----------
-    xdmf_file:
-        The XDMF file
-    mesh:
-        The dolfin mesh
-    mts: optional
-        The dict of MeshTags
-
-    """
-    logger = logging.getLogger("dolfiny")
-
-    logger.info("Writing mesh")
-    xdmf_file.write_mesh(mesh)
-
-    if mts is None:
-        return
-
-    keys_meshtags = {key: mt.dim for key, mt in mts.items()}
-
-    logger.info("Writing tag keys")
-    xdmf_file.write_information("KeysOfMeshTags", str(keys_meshtags))
-
-    logger.info("Writing meshtags")
-    for mt in mts.values():
-        mesh.topology.create_connectivity(mt.dim, mesh.topology.dim)
-        xdmf_file.write_meshtags(mt)
+import dolfinx.io
 
 
-def read(xdmf_file):
-    """Read mesh and meshtags from XDMFFile
+class XDMFFile(dolfinx.io.XDMFFile):
 
-    Parameters
-    ----------
-    xdmf_file:
-        The msh file
+    KEYS_OF_MESHTAGS = 'KeysOfMeshTags'
 
-    Returns
-    -------
-    mesh:
-        The dolfin mesh
-    mts:
-        The dict of meshtags
+    def write_mesh_meshtags(self, mesh, mts=None):
+        """Write mesh and meshtags to XDMFFile
 
-    """
+        Parameters
+        ----------
+        mesh:
+            The dolfin mesh
+        mts: optional
+            The dict of MeshTags
 
-    logger = logging.getLogger("dolfiny")
+        """
+        logger = logging.getLogger("dolfiny")
 
-    logger.info("Reading mesh")
-    mesh = xdmf_file.read_mesh(name="mesh")
+        logger.info("Writing mesh")
+        self.write_mesh(mesh)
 
-    mesh.topology.create_connectivity_all()
+        if mts is None:
+            return
 
-    logger.info("Reading tag keys")
-    value = xdmf_file.read_information("KeysOfMeshTags")
+        keys_meshtags = {key: mt.dim for key, mt in mts.items()}
 
-    import ast
-    keys_meshtags = ast.literal_eval(value)
+        logger.info("Writing information")
+        self.write_information(self.KEYS_OF_MESHTAGS, str(keys_meshtags))
 
-    mts = {}
+        logger.info("Writing meshtags")
+        for mt in mts.values():
+            mesh.topology.create_connectivity(mt.dim, mesh.topology.dim)
+            self.write_meshtags(mt)
 
-    logger.info("Reading meshtags")
-    for key in keys_meshtags.keys():
-        mts[key] = xdmf_file.read_meshtags(mesh, key)
+    def read_mesh_meshtags(self, mesh_name='mesh'):
+        """Read mesh and meshtags from XDMFFile
 
-    return mesh, mts
+        Parameters
+        ----------
+        mesh_name: optional
+            Name of the Grid node containing the mesh data in XDMF file
+
+        Returns
+        -------
+        mesh:
+            The dolfin mesh
+        mts:
+            The dict of meshtags
+
+        """
+
+        logger = logging.getLogger("dolfiny")
+
+        logger.info("Reading mesh")
+        mesh = self.read_mesh(name=mesh_name)
+
+        mesh.topology.create_connectivity_all()
+
+        logger.info("Reading information")
+        value = self.read_information(self.KEYS_OF_MESHTAGS)
+
+        import ast
+        keys_meshtags = ast.literal_eval(value)
+
+        mts = {}
+
+        logger.info("Reading meshtags")
+        for key in keys_meshtags.keys():
+            mts[key] = self.read_meshtags(mesh, key)
+
+        return mesh, mts
