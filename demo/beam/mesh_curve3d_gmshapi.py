42662c13e0e36da67e308d07ba968a61eb5ebaf9
Andreas Zilian <andreas.zilian@uni.lu>
2020-05-27 18:59:38 +0200

    Clean and align curve meshing

--- demo/beam/mesh_curve3d_gmshapi.py
+++ demo/beam/mesh_curve3d_gmshapi.py
@@ -1,167 +1,81 @@
 #!/usr/bin/env python3
 
+from mpi4py import MPI
 
-def mesh_curve3d_gmshapi(name="curve3d", shape="xline", L=1.0, nL=10, progression=1.0):
+
+def mesh_curve3d_gmshapi(name="curve3d", shape="xline", L=1.0, nL=10,
+                         progression=1.0, order=1, msh_file=None, comm=MPI.COMM_WORLD):
     """
-    Create mesh of 2d curve using the Python API of Gmsh.
+    Create mesh of 3d curve using the Python API of Gmsh.
     """
-    
-    tdim = 1 # target topological dimension
-    gdim = 3 # target geometrical dimension
 
-    # --- generate geometry and mesh with gmsh
+    tdim = 1  # target topological dimension
 
-    import gmsh
+    # Perform Gmsh work only on rank = 0
 
-    gmsh.initialize()
-    gmsh.option.setNumber("General.Terminal", 1)
+    if comm.rank == 0:
 
-    gmsh.model.add(name)
+        import gmsh
 
-    # create points and line
-    if shape == "xline":
-        p0 = gmsh.model.geo.addPoint(0.0, 0.0, 0.0)
-        p1 = gmsh.model.geo.addPoint(  L, 0.0, 0.0)
-        l0 = gmsh.model.geo.addLine(p0, p1)
-    elif shape == "zline":
-        p0 = gmsh.model.geo.addPoint(0.0, 0.0, 0.0)
-        p1 = gmsh.model.geo.addPoint(0.0, 0.0,   L)
-        l0 = gmsh.model.geo.addLine(p0, p1)
-    elif shape == "quarc":
-        R = 2.0*L/3.1415
-        p0 = gmsh.model.geo.addPoint(0.0, 0.0,   R)
-        p1 = gmsh.model.geo.addPoint(  R, 0.0, 0.0)
-        pc = gmsh.model.geo.addPoint(0.0, 0.0, 0.0)
-        l0 = gmsh.model.geo.addCircleArc(p0, pc, p1)
-    else:
-        print("Error: Unknown shape identifier '{:s}'.".format(shape))
-        exit()
+        # Initialise gmsh and set options
+        gmsh.initialize()
+        gmsh.option.setNumber("General.Terminal", 1)
 
-    # define physical groups for subdomains (! target tag > 0)
-    tag_domain = 1
-    gmsh.model.addPhysicalGroup(tdim, [l0], tag_domain)  # all lines
-    gmsh.model.setPhysicalName(tdim, tag_domain, 'subdomain_all')
+        # Add model under given name
+        gmsh.model.add(name)
 
-    # determine interfaces
-    # ... (not needed, we have the end points already)
+        # Create points and line
+        if shape == "xline":
+            p0 = gmsh.model.geo.addPoint(0.0, 0.0, 0.0)
+            p1 = gmsh.model.geo.addPoint(L, 0.0, 0.0)
+            l0 = gmsh.model.geo.addLine(p0, p1)
+        elif shape == "zline":
+            p0 = gmsh.model.geo.addPoint(0.0, 0.0, 0.0)
+            p1 = gmsh.model.geo.addPoint(0.0, 0.0, L)
+            l0 = gmsh.model.geo.addLine(p0, p1)
+        elif shape == "quarc":
+            R = 2.0 * L / gmsh.pi
+            p0 = gmsh.model.geo.addPoint(0.0, 0.0, R)
+            p1 = gmsh.model.geo.addPoint(R, 0.0, 0.0)
+            pc = gmsh.model.geo.addPoint(0.0, 0.0, 0.0)
+            l0 = gmsh.model.geo.addCircleArc(p0, pc, p1)
+        else:
+            raise RuntimeError("Unknown shape identifier \'{shape:s}\'")
 
-    # define physical groups for interfaces (! target tag > 0)
-    left = 1
-    gmsh.model.addPhysicalGroup(tdim-1, [p0], left)
-    gmsh.model.setPhysicalName(tdim-1, left, 'interface_left')
-    right = 2
-    gmsh.model.addPhysicalGroup(tdim-1, [p1], right)
-    gmsh.model.setPhysicalName(tdim-1, right, 'interface_right')
+        # Define physical groups for subdomains (! target tag > 0)
+        domain = 1
+        gmsh.model.addPhysicalGroup(tdim, [l0], domain)  # all lines
+        gmsh.model.setPhysicalName(tdim, domain, 'domain')
 
-    # check and store labels
-    labels = {}
-    for tdim in range(tdim+1):
-        print("gmsh physical groups (topological dimension = {:1d}):".format(tdim))
-        for info in gmsh.model.getPhysicalGroups(tdim):
-            dim = info[0]
-            tag = info[1]
-            gid = gmsh.model.getPhysicalName(tdim, info[1])
-            print("dim = {:1d} | tag = {:3d} | physical name = {:s}".format(dim, tag, gid))
-            labels[gid] = tag
-    
-    # sync
-    gmsh.model.geo.synchronize()
+        # Determine interfaces
+        # ... (not needed, we have the end points already)
 
-    # set refinement along curve direction
-    gmsh.model.mesh.setTransfiniteCurve(l0, numNodes=nL, meshType="Progression", coef=progression)
+        # Define physical groups for interfaces (! target tag > 0)
+        left = 1
+        gmsh.model.addPhysicalGroup(tdim - 1, [p0], left)
+        gmsh.model.setPhysicalName(tdim - 1, left, 'left')
+        right = 2
+        gmsh.model.addPhysicalGroup(tdim - 1, [p1], right)
+        gmsh.model.setPhysicalName(tdim - 1, right, 'right')
 
-    # generate the mesh
-    gmsh.model.mesh.generate()
+        # Sync
+        gmsh.model.geo.synchronize()
 
-    # write the mesh
-    gmsh.write("/tmp/" + name + ".msh")
+        # Set refinement along curve direction
+        gmsh.model.mesh.setTransfiniteCurve(l0, numNodes=nL, meshType="Progression", coef=progression)
 
-    gmsh.finalize()
+        # Generate the mesh
+        gmsh.model.mesh.generate()
 
-    # --- convert mesh with meshio
+        # Set geometric order of mesh cells
+        gmsh.model.mesh.setOrder(order)
 
-    import meshio
+        # Optional: Write msh file
+        if msh_file is not None:
+            gmsh.write(msh_file)
 
-    print("Reading Gmsh mesh into meshio")
-    mesh = meshio.read("/tmp/" + name + ".msh")
-    # mesh.prune()
-
-    points_pruned = mesh.points[:, :gdim]  # set active coordinate components
-
-    cell_types = { 3: ["tetra", "hex"], 2: ["triangle", "quad"], 1: ["line"], 0: ["vertex"] }
-
-    print("Writing mesh for dolfin Mesh")
-    meshio.write("/tmp/" + name + ".xdmf", meshio.Mesh(
-        points=points_pruned,
-        cells={key: mesh.cells[key] for key in cell_types[tdim] if key in mesh.cells}
-    ))
-
-    print("Writing subdomain data for dolfin MeshValueCollection")
-    meshio.write("/tmp/" + name + "_subdomains" + ".xdmf", meshio.Mesh(
-        points=points_pruned,
-        cells={key: mesh.cells[key] for key in cell_types[tdim] if key in mesh.cells},
-        cell_data={key: {"name_to_read": mesh.cell_data[key]["gmsh:physical"]}
-                   for key in cell_types[tdim] 
-                   if key in mesh.cell_data and "gmsh:physical" in mesh.cell_data[key]}
-    ))
-
-    print("Writing interface data for dolfin MeshValueCollection")
-    meshio.write("/tmp/" + name + "_interfaces" + ".xdmf", meshio.Mesh(
-        points=points_pruned,
-        cells={key: mesh.cells[key] for key in cell_types[tdim-1] if key in mesh.cells},
-        cell_data={key: {"name_to_read": mesh.cell_data[key]["gmsh:physical"]} 
-                   for key in cell_types[tdim-1] 
-                   if key in mesh.cell_data and "gmsh:physical" in mesh.cell_data[key]}
-    ))
-
-    # --- test mesh with dolfin and write
-
-    import dolfin as df
-    import dolfin.io as dfio
-
-    # plain mesh
-    print("Reading mesh into dolfin")
-    with dfio.XDMFFile(df.MPI.comm_world, "/tmp/" + name + ".xdmf") as infile:
-        mesh = infile.read_mesh(df.cpp.mesh.GhostMode.none)
-    infile.close()
-
-    tdim = mesh.topology.dim
-    gdim = mesh.geometry.dim
-    print(" --- dolfin: topological dimension = {}".format(tdim))
-    print(" --- dolfin: geometrical dimension = {}".format(gdim))
-
-    print("Writing mesh from dolfin")
-    with dfio.XDMFFile(df.MPI.comm_world, name + ".xdmf") as outfile:
-        outfile.write(mesh)
-
-    # subdomains
-    print("Reading subdomains into dolfin")
-    with dfio.XDMFFile(df.MPI.comm_world, "/tmp/" + name + "_subdomains" + ".xdmf") as infile:
-        msh_subdomains = mesh
-        mvc_subdomains = infile.read_mvc_size_t(msh_subdomains)
-    infile.close()
-
-    mfc_subdomains = df.cpp.mesh.MeshFunctionSizet(msh_subdomains, mvc_subdomains, 0)
-
-    print("Writing subdomains from dolfin")
-    with dfio.XDMFFile(df.MPI.comm_world, name + "_subdomains" + ".xdmf") as outfile:
-        outfile.write(mfc_subdomains)
-
-    # boundaries
-    print("Reading interfaces into dolfin")
-    with dfio.XDMFFile(df.MPI.comm_world, "/tmp/" + name + "_interfaces" + ".xdmf") as infile:
-        msh_interfaces = mesh
-        mvc_interfaces = infile.read_mvc_size_t(msh_interfaces)
-    infile.close()
-
-    mfc_interfaces = df.cpp.mesh.MeshFunctionSizet(msh_interfaces, mvc_interfaces, 0)
-
-    print("Writing interfaces from dolfin")
-    with dfio.XDMFFile(df.MPI.comm_world, name + "_interfaces" + ".xdmf") as outfile:
-        outfile.write(mfc_interfaces)
-
-    return mesh, labels
+    return gmsh.model if comm.rank == 0 else None, tdim
 
 
 if __name__ == "__main__":
-    mesh_curve3d_gmshapi()
+    mesh_curve3d_gmshapi(msh_file="curve3d.msh")
