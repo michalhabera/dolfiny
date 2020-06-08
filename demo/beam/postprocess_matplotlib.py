a6635b818697a36ebdff068d5920f421703dcfc8
Andreas Zilian <andreas.zilian@uni.lu>
2020-06-08 16:15:04 +0200

    Add custom matplotlib plotter for beam demo

---
+++ demo/beam/postprocess_matplotlib.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+
+class Plotter():
+
+    def __init__(self, name):
+
+        self.name = name
+
+        import matplotlib.pyplot
+
+        fig, ax1 = matplotlib.pyplot.subplots()
+        ax1.set_title(r'finite deformation beam (displacement-based)', fontsize=12)
+        ax1.set_xlabel(r'coordinate $x$, displacement $[m]$', fontsize=12)
+        ax1.set_ylabel(r'coordinate $z$, displacement $[m]$', fontsize=12)
+        ax1.invert_yaxis()
+        ax1.axis('equal')
+        ax1.grid(linewidth=0.25)
+
+        fig.tight_layout()
+
+        self.fig = fig
+        self.ax1 = ax1
+
+    def add(self, mesh, q, m, μ):
+
+        x0, (ui, wi, ri) = self.interpolate_on_mesh(mesh, q, m)
+
+        color = (0.5 + μ.value * 0.5, 0.5 - μ.value * 0.5, 0.5 - μ.value * 0.5)
+        endco = (0.5 - μ.value * 0.5, 0.5 - μ.value * 0.5, 0.5 + μ.value * 0.5)
+        label = 'undeformed' if μ.value == 0.0 else 'deformed' if μ.value == 1.0 else None
+
+        self.ax1.plot(x0[:, 0] + ui, x0[:, 2] + wi, '.-', linewidth=0.75, markersize=2.0, color=color, label=label)
+        self.ax1.plot(x0[-1, 0] + ui[-1], x0[-1, 2] + wi[-1], 'o', markersize=3.0, color=endco)  # mark end
+        self.ax1.legend(loc='upper left')
+
+        self.fig.savefig(self.name + '_result.pdf')
+
+    def interpolate_on_mesh(self, mesh, q, u):
+
+        # Extract mesh geometry nodal coordinates
+        dm = mesh.geometry.dofmap
+        oq = [0] + [*range(2, q + 1)] + [1]  # reorder lineX nodes: all ducks in a row...
+        x0_idx = [[dm.links(i).tolist()[k] for k in oq] for i in range(dm.num_nodes)]
+        x0_idx = [item for sublist in x0_idx for item in sublist]
+        x0 = mesh.geometry.x[x0_idx]
+
+        # Interpolate solution at mesh geometry nodes
+        import dolfinx
+        import dolfiny.interpolation
+        Q = dolfinx.FunctionSpace(mesh, ("P", q))
+        uf = dolfinx.Function(Q)
+
+        if isinstance(u, list):
+            ui = []
+            for u_ in u:
+                dolfiny.interpolation.interpolate(u_, uf)
+                ui.append(uf.vector[x0_idx])
+        else:
+            dolfiny.interpolation.interpolate(u, uf)
+            ui = uf.vector[x0_idx]
+
+        return x0, ui
