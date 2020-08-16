1827837a1d503a5560eb0db3b1227f79155e0c5c
Andreas Zilian <andreas.zilian@uni.lu>
2020-08-16 20:13:29 +0200

    Test ODEInt for solution and convergence

--- test/test_odeint.py
+++ test/test_odeint.py
@@ -123,74 +123,7 @@ def ode_1st_order_galpha(a=1.0, b=0.5, u_0=1.0, nT=10, dt=0.1, **kwargs):
     return u
 
 
-# def ode_1st_order_odeint(a=1.0, b=0.0, u_0=1.0, nT=10, **kwargs):
-#     """
-#     Create 1st order ODE problem and solve with `ODEInt` time integrator.
-
-#     First order ODE: dot u + a * u = b with initial condition u(t=0) = u_0
-#     """
-
-#     mesh = UnitCubeMesh(MPI.COMM_WORLD, 1, 1, 1)
-#     U = FunctionSpace(mesh, ("DG", 0))
-
-#     u = Function(U, name="u")
-#     u0 = Function(U, name="u0")
-#     u0t = Function(U, name="u0t")
-
-#     u0.vector.set(u_0)  # initial condition
-#     u0t.vector.set(b - a * u_0)  # exact initial rate of this ODE for generalised alpha
-
-#     δu = ufl.TestFunction(U)
-
-#     dx = ufl.Measure("dx", domain=mesh)
-
-#     # Global time
-#     time = dolfinx.Constant(mesh, 0.0)
-
-#     # Time integrator
-#     odeint = dolfiny.odeint.ODEInt(x=u, x0=u0, x0t=u0t, **kwargs)
-
-#     # Weak form: time-rate-dependent components (as one-form)
-#     @odeint.form_hook
-#     def g(dudt):
-#         g = δu * dudt * dx
-#         return g
-
-#     # Weak form: time-rate-independent components (as one-form)
-#     @odeint.form_hook
-#     def f(u):
-#         f = δu * a * u * dx - δu * b * dx
-#         return f
-
-#     # Overall form (as one-form)
-#     F = odeint.discretise_in_time(g, f)
-#     # Overall form (as list of forms)
-#     F = dolfiny.function.extract_blocks(F, [δu])
-
-#     # Create problem
-#     problem = dolfiny.snesblockproblem.SNESBlockProblem(F, [u])
-
-#     u_avg = numpy.zeros(nT + 1)
-#     u_avg[0] = u0.vector.norm(0) / u0.vector.getSize()
-
-#     # Process time steps
-#     for time_step in range(1, nT + 1):
-
-#         # Set current time
-#         time.value = time_step * odeint.dt
-
-#         # Solve nonlinear problem
-#         u, = problem.solve()
-
-#         # Update solution states for time integration
-#         odeint.update()
-
-#         u_avg[time_step] = u.vector.norm(0) / u.vector.getSize()
-
-#     return u_avg
-
-
-def ode_1st_order_odeint(a=1.0, b=0.0, u_0=1.0, nT=10, **kwargs):
+def ode_1st_order_odeint(a=1.0, b=0.0, u_0=1.0, nT=10, dt=0.1, **kwargs):
     """
     Create 1st order ODE problem and solve with `ODEInt` time integrator.
 
@@ -203,11 +136,8 @@ def ode_1st_order_odeint(a=1.0, b=0.0, u_0=1.0, nT=10, **kwargs):
     u = Function(U, name="u")
     ut = Function(U, name="ut")
 
-    u0 = Function(U, name="u0")
-    u0t = Function(U, name="u0t")
-
-    u0.vector.set(u_0)  # initial condition
-    u0t.vector.set(b - a * u_0)  # exact initial rate of this ODE for generalised alpha
+    u.vector.set(u_0)  # initial condition
+    ut.vector.set(b - a * u_0)  # exact initial rate of this ODE for generalised alpha
 
     δu = ufl.TestFunction(U)
 
@@ -217,16 +147,16 @@ def ode_1st_order_odeint(a=1.0, b=0.0, u_0=1.0, nT=10, **kwargs):
     time = dolfinx.Constant(mesh, 0.0)
 
     # Time step size
-    dt = dolfinx.Constant(mesh, kwargs["dt"])
+    dt = dolfinx.Constant(mesh, dt)
 
     # Time integrator
-    odeint = dolfiny.odeint.ODEInt(t=time, dt=dt, x=u, xt=ut, x0=u0, x0t=u0t)  # TODO: **kwargs
+    odeint = dolfiny.odeint.ODEInt(t=time, dt=dt, x=u, xt=ut, **kwargs)
 
     # Weak form (as one-form)
     f = δu * ut * dx + δu * a * u * dx - δu * b * dx
 
     # Overall form (as one-form)
-    F = odeint.discretise_in_time(f, u, ut)
+    F = odeint.discretise_in_time(f)
     # Overall form (as list of forms)
     F = dolfiny.function.extract_blocks(F, [δu])
 
@@ -234,7 +164,7 @@ def ode_1st_order_odeint(a=1.0, b=0.0, u_0=1.0, nT=10, **kwargs):
     problem = dolfiny.snesblockproblem.SNESBlockProblem(F, [u])
 
     u_avg = numpy.zeros(nT + 1)
-    u_avg[0] = u0.vector.norm(0) / u0.vector.getSize()
+    u_avg[0] = u.vector.norm(0) / u.vector.getSize()
 
     # Process time steps
     for time_step in range(1, nT + 1):
@@ -262,8 +192,6 @@ def test_odeint_highlevel():
     u_expected = ode_1st_order_eulerf(a=1.0, b=0.5, u_0=1.0, nT=10, dt=0.1)
     assert(numpy.isclose(u_computed, u_expected, rtol=1.0e-12).all())
 
-    return
-
     # Euler backward
     u_computed = ode_1st_order_odeint(a=1.0, b=0.5, u_0=1.0, nT=10, dt=0.1, alpha_f=1.0, alpha_m=0.5, gamma=0.5)
     u_expected = ode_1st_order_eulerb(a=1.0, b=0.5, u_0=1.0, nT=10, dt=0.1)
@@ -281,7 +209,7 @@ def test_odeint_highlevel():
 
     # (2) Check wrt convergence order
 
-    # Generalised alpha
+    # Parameter/order sets
     mi = {'euler_forward': {'param': {'alpha_f': 0.0, 'alpha_m': 0.5, 'gamma': 0.5}, 'order': 1},
           'euler_backward': {'param': {'alpha_f': 1.0, 'alpha_m': 0.5, 'gamma': 0.5}, 'order': 1},
           'crank_nicolson': {'param': {'alpha_f': 0.5, 'alpha_m': 0.5, 'gamma': 0.5}, 'order': 2},
