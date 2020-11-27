e9cdd2cbc837a3c41331a3ec40163b0e7cf1ebeb
Michal Habera <michal.habera@gmail.com>
2020-11-27 08:37:18 +0100

    Try DG1 test

--- test/test_interpolation.py
+++ test/test_interpolation.py
@@ -16,7 +16,7 @@ DG1 = FunctionSpace(mesh, ("DG", 1))
 CG1 = FunctionSpace(mesh, ("CG", 1))
 TCG1 = TensorFunctionSpace(mesh, ("CG", 1))
 TDG0 = TensorFunctionSpace(mesh, ("DG", 0))
-TDG0s = TensorFunctionSpace(mesh, ("DG", 0), symmetry=True)
+TDG0s = TensorFunctionSpace(mesh, ("DG", 1), symmetry=True)
 
 CG2 = FunctionSpace(mesh, ("CG", 2))
 VCG1 = FunctionSpace(mesh, DG0.mesh.ufl_domain().ufl_coordinate_element())
@@ -55,10 +55,10 @@ def test_expr():
     assert h_project2.vector.norm() < 0.1
 
     expr3 = h_interp
-    h_interp3 = Function(TDG0s)
+    h_interp3 = Function(TDG1s)
     interpolation.interpolate(expr3, h_interp3)
 
-    h_project3 = Function(TDG0s)
+    h_project3 = Function(TDG1s)
     projection.project(expr3, h_project3)
 
     h_project3.vector.axpy(-1.0, h_interp3.vector)
