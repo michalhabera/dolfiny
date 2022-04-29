d33a6e77bd6abab8a269c50589a7bd6f866b953b
Michal Habera <michal.habera@gmail.com>
2022-04-29 13:56:12 +0200

    Add initial sketch

--- dolfiny/__init__.py
+++ dolfiny/__init__.py
@@ -2,9 +2,9 @@ import logging
 
 logger = logging.Logger("dolfiny")
 
-from dolfiny import (expression, function, interpolation, io, la, mesh, odeint,
+from dolfiny import (expression, function, interpolation, io, la, localsolver, mesh, odeint,
                      projection, restriction, slepcblockproblem,
                      snesblockproblem)
 
-__all__ = ["expression", "function", "io", "interpolation", "la", "mesh",
+__all__ = ["expression", "function", "io", "interpolation", "la", "localsolver", "mesh",
            "odeint", "projection", "restriction", "slepcblockproblem", "snesblockproblem"]
