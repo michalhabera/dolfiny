ae33c53f367ab0bfefbf7668972ff4d4a7d82405
Andreas Zilian <andreas.zilian@uni.lu>
2023-07-17 19:42:35 +0000

    Fix pprint args and add helper functions

--- dolfiny/utils.py
+++ dolfiny/utils.py
@@ -18,4 +18,43 @@ def pprint(str="", end="", flush=True, comm=MPI.COMM_WORLD):
     """
 
     if comm.rank == 0:
-        print(str, end, flush=True)
+        print(str, end, flush=flush)
+
+
+def attributes_to_dict(c, invert=False):
+    """Generate dictionary of class attributes.
+
+    Parameters
+    ----------
+    s: Class
+    invert: optional
+        Invert key-value pair in dictionary
+
+    """
+    d = {}
+
+    for k, v in vars(c).items():
+        if not callable(v) and not k.startswith('__'):
+            if invert:
+                d[v] = k
+            else:
+                d[k] = v
+
+    return d
+
+
+def prefixify(n: int, prefixes=[' ', 'k', 'm', 'b']) -> str:
+    """Convert given integer number to [sign] + 3 digits + (metric) prefix.
+
+    Parameters
+    ----------
+    n: integer
+    prefixes: optional
+        List of (metric) prefix characters
+
+    """
+    # https://stackoverflow.com/a/74099536
+    i = int(0.30102999566398114 * (int(n).bit_length() - 1)) + 1
+    e = i - (10 ** i > n)
+    e //= 3
+    return f"{n // 10**(3 * e):>3d}{prefixes[e]}"
